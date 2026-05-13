10be90a128e177b52a43148724809f891cafbcd6
usdtharvester-jpg <usdtharvester@gmail.com>
2026-05-13 18:49:31 +0530

    tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -10,11 +10,11 @@ async def start(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text(
-        "🚀 Welcome to BG Bot!\n\n"
+        "🚀 Welcome to BG Wealth Sharing Bot!\n\n"
         "👉 Join our community & watch updates:",
         reply_markup=reply_markup
     )
 
-app = ApplicationBuilder().token("8311716950:AAGgO3h3GoGgKhNNTeAYF3TWtWQPA0uT7oM").build()
+app = ApplicationBuilder().token("8311716950:AAEqLgTvulrep7a7u5A_7wk2mplSeBIyMt0").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
